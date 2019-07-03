21e023c5938aae7a7cafb273e5a39cb4800dbdad
Vasileios Mavroeidis <vasileim@ifi.uio.no>
2019-07-03 12:35:05 +0200

    Update ipnet.R
    Additional Functionality: Given an IP without subnet mask or slash network bits (CIDR - e.g., /24) is treaded as /32 with subnet mask 255.255.255.255. The wildcard mask of this example would be 0.0.0.0 that defines a single IP.

--- ipnet.R
+++ ipnet.R
@@ -1,15 +1,15 @@
 
 ipnet <- function(address=NULL) {
-        list_of_packages <- c("xlsx", "stringr", "dplyr")
+        list_of_packages <- c("stringr", "dplyr")
         for (p in list_of_packages) {if(p %in% rownames(installed.packages()) == FALSE) {
-                install.packages(p)
+                install.packages(p, repos = "http://cran.us.r-project.org")
                 library(p, character.only = TRUE)} else library(p, character.only = TRUE)
         }
         
-        #vector for calculating the wildcard (s-subnet mask)
+        #Vector for calculating the wildcard (s-subnet mask)
         s=c(255,255,255,255)
         
-        #read the r_ipnet subnettting file from Google Drive
+        #Read the r_ipnet subnettting file from Google Drive
         id <- "1Bzbb9OV8oKax-OF7HamndGX7faV-b717" # google file ID
         subnet_table <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
         
@@ -17,17 +17,18 @@ ipnet <- function(address=NULL) {
         if (is.null(address)==TRUE){
                 address <- as.character(readline(prompt="Enter IP address and Subnet Mask (with a space in between) or CIDR: "))
         }
-        
-        
-        #Regular Expression when the user submits CIDR (address1 and first if statement) or IP and Subnet Mask (address2 and else if statement). If the input is wrong print it out.
-        if (!is.na(str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\/[[:digit:]]{1,2}"))) {
-                address1 <- str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\/[[:digit:]]{1,2}")   
+        #Regular Expression when the user submits CIDR (address1 and first if statement) or IP and Subnet Mask (address2 and else if statement). If the function gets an IP address without specifying CIDR or subnet mask is treated as "32" and assigned to address1 variable.
+        if (!is.na(str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\/[[:digit:]]{1,2}$"))) {
+                address1 <- str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\/[[:digit:]]{1,2}$")   
                 print("Address and Subnet Mask from CIDR were EXTRACTED successfully (1)")
         } else if (!is.na(str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}[\\s][[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}"))) {
                 address2 <-  str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}[\\s][[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}")
                 print("Address and Subnet Mask were EXTRACTED successfully (2)")
-        } else print("Wrong Input")
-        #Here to calculate the usable hosts we could also substract the prefix size from 32 and then result is the power that we need to raise 2 (e.g.,  for /24 -> 32-24= 8, and then we raise 2 to the power of 8 = 256 hosts) 
+        } else if (!is.na(str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}$"))){
+                address1 <- str_extract(address, "[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}\\.[[:digit:]]{1,3}$")
+                address1 <- paste0(address1,"/32")
+        }
+        #To calculate the usable hosts we could also substract the prefix size from 32 and then result is the power that we need to raise 2 (e.g.,  for /24 -> 32-24= 8, and then we raise 2 to the power of 8 = 256 hosts) 
         if (exists("address1") == TRUE) {
                 address <- strsplit(address1, "/", fixed = TRUE)
                 subnet_info_from_table_extracted <- filter(subnet_table, subnet_table$prefix==address[[1]][2])
@@ -56,7 +57,7 @@ ipnet <- function(address=NULL) {
         binary_subnet <<- dec_to_binary(subnet_mask_for_binary_function)
 }
 
-#this function transforms decimal to binary format (representation)
+#Transforms decimal to binary format (representation)
 dec_to_binary <- function(v) { 
         ip <-  strsplit(v,".", fixed = TRUE)
         ip <- as.integer(unlist(ip))
@@ -78,7 +79,7 @@ dec_to_binary <- function(v) {
         return(string)
 }
 
-#function to print IPv4 related information based on the given CIDR or Subnet mask
+#Prints IPv4 related information based on the given CIDR or Subnet mask
 print.ipnet <- function() {
         print(paste0("Address: ", ip))
         print(paste0("Netmask: ", netmask))
